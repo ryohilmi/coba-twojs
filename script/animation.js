2268093e1f35f4cb44a0f555c1bcd55e0da35416
Ryo Hilmi <ryodm.74@gmail.com>
2022-04-11 01:02:50 +0700

    feat: add robot factory

--- script/animation.js
+++ script/animation.js
@@ -1,4 +1,5 @@
 import Two from "two.js";
+import svg from "../pisang.svg";
 
 let arrow;
 let two;
@@ -8,6 +9,45 @@ let rightDown = false;
 let forwardDown = false;
 let backwardDown = false;
 
+function createRobot(two, x, y, insideColor, outsideColor) {
+  const circle1Radius = 50;
+
+  const outerCircle = two.makeCircle(x, y, circle1Radius + 8);
+  outerCircle.fill = outsideColor;
+  outerCircle.linewidth = 0;
+
+  const tusuk = two.makePolygon(0, 0, 50, 3);
+  tusuk.fill = outsideColor;
+  tusuk.linewidth = 0;
+  tusuk.position.x = x;
+  tusuk.position.y = y - 60;
+
+  const innerCircle = two.makeCircle(x, y, circle1Radius);
+  innerCircle.fill = insideColor;
+  innerCircle.linewidth = 0;
+
+  const baseX = x;
+  const baseY = y - 100;
+
+  const leftLine = two.makeLine(baseX, baseY, baseX + 75, baseY + 100);
+  leftLine.stroke = outsideColor;
+  leftLine.linewidth = 10;
+
+  const rightLine = two.makeLine(baseX, baseY, baseX - 75, baseY + 100);
+  rightLine.stroke = outsideColor;
+  rightLine.linewidth = 10;
+
+  const robot = two.makeGroup(
+    outerCircle,
+    tusuk,
+    innerCircle,
+    leftLine,
+    rightLine
+  );
+
+  return robot;
+}
+
 function init() {
   let params = {
     fullscreen: true,
@@ -16,13 +56,16 @@ function init() {
   const elem = document.body;
   two = new Two(params).appendTo(elem);
 
+  const robot = createRobot(two, 400, 300, "#dd0000", "#ff0000");
+  const robot2 = createRobot(two, 700, 300, "#00cc00", "#00ff00");
+
   const circleRadius = 50;
   const circle = two.makeCircle(0, 0, circleRadius);
 
   circle.fill = "#FF8000";
   circle.linewidth = 0;
 
-  const triangle = two.makePolygon(0, 0, 50, 3);
+  const triangle = two.makePolygon(30, 50, 30, 3);
 
   triangle.fill = "#FF8000";
   triangle.linewidth = 0;
