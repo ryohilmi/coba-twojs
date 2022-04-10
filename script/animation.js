aca6c72ee2c048e28c7bb1c8c0592ac44b98ba57
Ryo Hilmi <ryodm.74@gmail.com>
2022-04-11 01:13:15 +0700

    feat: add robot number

--- script/animation.js
+++ script/animation.js
@@ -9,40 +9,47 @@ let rightDown = false;
 let forwardDown = false;
 let backwardDown = false;
 
-function createRobot(two, x, y, insideColor, outsideColor) {
+function createRobot(two, number, x, y, insideColor, outsideColor) {
   const circle1Radius = 50;
 
   const outerCircle = two.makeCircle(x, y, circle1Radius + 8);
   outerCircle.fill = outsideColor;
   outerCircle.linewidth = 0;
 
-  const tusuk = two.makePolygon(0, 0, 50, 3);
+  const tusuk = two.makePolygon(0, 0, 40, 3);
   tusuk.fill = outsideColor;
   tusuk.linewidth = 0;
   tusuk.position.x = x;
   tusuk.position.y = y - 60;
 
+  const baseX = x;
+  const baseY = y - 100;
+
+  const rightLine = two.makeLine(baseX, baseY + 10, baseX + 80, baseY + 110);
+  rightLine.stroke = outsideColor;
+  rightLine.linewidth = 10;
+
+  const leftLine = two.makeLine(baseX, baseY + 10, baseX - 80, baseY + 110);
+  leftLine.stroke = outsideColor;
+  leftLine.linewidth = 10;
+
   const innerCircle = two.makeCircle(x, y, circle1Radius);
   innerCircle.fill = insideColor;
   innerCircle.linewidth = 0;
 
-  const baseX = x;
-  const baseY = y - 100;
-
-  const leftLine = two.makeLine(baseX, baseY, baseX + 75, baseY + 100);
-  leftLine.stroke = outsideColor;
-  leftLine.linewidth = 10;
-
-  const rightLine = two.makeLine(baseX, baseY, baseX - 75, baseY + 100);
-  rightLine.stroke = outsideColor;
-  rightLine.linewidth = 10;
+  const id = two.makeText(number, baseX, baseY + 105, {
+    size: 40,
+    fill: "#ffffff",
+    weight: 600,
+  });
 
   const robot = two.makeGroup(
     outerCircle,
     tusuk,
-    innerCircle,
     leftLine,
-    rightLine
+    rightLine,
+    innerCircle,
+    id
   );
 
   return robot;
@@ -56,8 +63,8 @@ function init() {
   const elem = document.body;
   two = new Two(params).appendTo(elem);
 
-  const robot = createRobot(two, 400, 300, "#dd0000", "#ff0000");
-  const robot2 = createRobot(two, 700, 300, "#00cc00", "#00ff00");
+  const robot = createRobot(two, 1, 400, 300, "#dd0000", "#ff0000");
+  const robot2 = createRobot(two, 2, 700, 300, "#00cc00", "#00ff00");
 
   const circleRadius = 50;
   const circle = two.makeCircle(0, 0, circleRadius);
